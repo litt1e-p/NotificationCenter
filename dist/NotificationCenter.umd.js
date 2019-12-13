7ce2711ceed49681200d16e6ece92f51fc850c68
litt1e-p <free9fw@gmail.com>
2019-12-13 16:26:17 +0800

    [IMP]package lock make

--- dist/NotificationCenter.umd.js
+++ dist/NotificationCenter.umd.js
@@ -2,7 +2,7 @@
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
   typeof define === 'function' && define.amd ? define(['exports'], factory) :
   (global = global || self, factory(global.NotificationCenter = {}));
-}(this, function (exports) { 'use strict';
+}(this, (function (exports) { 'use strict';
 
   /*!
    * Vue.js v2.6.10
@@ -8434,4 +8434,4 @@
 
   Object.defineProperty(exports, '__esModule', { value: true });
 
-}));
+})));
